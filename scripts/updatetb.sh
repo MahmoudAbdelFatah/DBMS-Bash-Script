9462f5cfea09ef41cef758a8998d92d90451b007
Mahmoud AbdelFatah <abdelfatahm3@gmail.com>
2022-05-26 18:16:55 +0200

    add modify datatype and ls column names in updatetb

--- scripts/updatetb.sh
+++ scripts/updatetb.sh
@@ -1,32 +1,39 @@
 #!/bin/bash
 colDType=""
 
-removeColumn(){
+lsColNames() {
+    tname=$1
+    echo -e "${red}Name\tDT$end"
+    cat $path/.$tname.type | tr ":" "\t"
+
+}
+
+removeColumn() {
     tname=$1
 
     read -p "Enter the column name that u want to remove it: " colName
     checkname=$($scriptsPath/chkname.sh $colName)
-    if [ $checkname -eq 0 ] ;then
+    if [ $checkname -eq 0 ]; then
         #check if this colName is exist or not
         result=$(grep -cw ^$colName $path/.$tname.type)
 
-        if [ $result -gt 0 ] ;then
+        if [ $result -gt 0 ]; then
             #to get the line number of column name which will delete it
-            # awk 'match($0,v){print NR; exit}' v=$external_variable input-file
             lineNumber=$(awk -F: -v coln=$colName '{if(coln==$1) print NR }' $path/.$tname.type)
             echo "column number is: $lineNumber"
-            if [ $lineNumber -ne 1 ] ;then
+            if [ $lineNumber -ne 1 ]; then
                 #delete specific line number that contain the column name
                 sed -i "$lineNumber"'d' $path/.$tname.type
-                
+
+                #delete the associated data with this column
                 #--complement: keeps columns other than the ones specified in -f
                 outputAfterDel=$(cut --complement -d: -f$lineNumber $path/$tname)
-                
+
                 #redirect output of the undeleted columns to overwrite the file to store it
-                echo $outputAfterDel > $path/$tname
-                
+                echo $outputAfterDel >$path/$tname
+
                 echo "$red${bg}The column $colName is removed with its data Successfully.$end"
-            elif [ $lineNumber -eq 1 ] ;then
+            elif [ $lineNumber -eq 1 ]; then
                 echo "${red}The PK column can't be deleted.$end"
             fi
         else
@@ -35,47 +42,49 @@ removeColumn(){
     else
         removeColumn $tname
     fi
-    
+
 }
 
-modifyDatatype(){
+modifyDatatype() {
     #select using pk
     tname=$1
-    colNums=$(wc -l <$path/.$tname.type)
-    #get number of records in a file to loop searching for PK
-    recordNums=$(wc -l <$path/$tname)
+    colnames=($(cut -d: -f1 $path/.$tname.type))
 
-    #if the files is not empty
-    if [ $recordNums -gt 0 ]; then
-        #get data type of first column as it is the PK column
-        colDtype=($(head -1 $path/.$tname.type | cut -d: -f2))
-        read -p "Enter the PK of the column you want as a Data Type of $colDtype: " pkId
+    echo "The column names are: $red${colnames[@]}$end"
 
-        #get all records to loop searching for PK input
-        records=$(cat $path/$tname)
-        #loop on number of records to search for the input pk
-        if [ $colDtype == 'int' ]; then
-            check=$($scriptsPath/chkint.sh $pkId)
-        else
-            check=$($scriptsPath/chkname.sh $pkId)
-        fi
-        if [ $check -eq 0 ]; then
-            #search for required PK
-            isPk=$(grep -cw ^$pkId $path/$tname)
-            if [ $isPk -eq 1 ]; then
-                colName=$(cut -d: -f1 $path/.$tname.type | tr '\n' "\t")
-                echo -e "$red$colName$end"
-                sed -n "/^$pkId/p" $path/$tname | tr ':' '\t'
-                echo -e "-------------------------\n"
-            else
-                echo "${red}There is no ID match your input$end"
+    read -p "Enter the column name that u want to modify its datatype: " colName
+
+    coltype=($(awk -F: '{print  $2}' $path/.$tname.type))
+    checkname=$($scriptsPath/chkname.sh $colName)
+
+    if [ $checkname -eq 0 ]; then
+        #check if this colName is exist or not
+        result=$(grep -cw ^$colName $path/.$tname.type)
+
+        if [ $result -gt 0 ]; then
+            #get the line number of column name which will delete it
+            lineNumber=$(awk -F: -v coln=$colName '{if(coln==$1) print NR }' $path/.$tname.type)
+            echo "column number is: $lineNumber"
+            if [ $lineNumber -ne 1 ]; then
+                #check datatype before modify
+                i=$((lineNumber - 1))
+                if [ "${coltype[$i]}" == 'int' ]; then
+                    echo "${red}You can't modify this column, you can't convert from int to varchar datatype$end"
+                else
+                    #update data type if its int to be varchar
+                    #FS contains the file delimiter, by default the space is used to concatenate
+                    getoutput=$(awk -F: -v coln=$lineNumber '{if(NR==coln)$2="int";}1' OFS=: $path/.$tname.type)
+                    echo "$getoutput" >$path/.$tname.type
+                    echo "$red${bg}The column $colName is modified the new datatype Successfully.$end"
+                fi
+            elif [ $lineNumber -eq 1 ]; then
+                echo "${red}The PK column can't be Modified.$end"
             fi
         else
-            selectOneRecord $tname
+            echo "${red}There is no column with this name to modify its datatype.$end"
         fi
     else
-        echo "${red}No records to select from them. Using inesert to add Records firstly...$end"
-        return
+        modifyDatatype $tname
     fi
 
 }
@@ -114,12 +123,11 @@ checkUniqueColName() {
         record+="$colName:"
         selectDType
         record+="$colDType"
-        echo "new record is: $record"
-        echo -e "$record" >> $path/.$tname.type
+        echo -e "$record" >>$path/.$tname.type
         echo "$red${bg}the new column $colName with datatype of $colDType is added successfully.$end"
         #should check the file is not empty
         lineNumbers=$(wc -l <$path/$tname)
-        if [ $lineNumbers -gt 0 ] ;then
+        if [ $lineNumbers -gt 0 ]; then
             sed 's/$/:/' -i $path/$tname
         fi
     fi
@@ -130,7 +138,7 @@ addNewColumn() {
 
     read -p "Please Enter the new column name: " colName
     checkName=$($scriptsPath/chkname.sh $colName)
-    if [ $checkName -eq 0 ] ;then
+    if [ $checkName -eq 0 ]; then
         #check column name is unique.
         checkUniqueColName $tname $colName
     fi
@@ -139,7 +147,7 @@ addNewColumn() {
 
 updateMenu() {
     tname=$1
-    select reply in "Add New Column." "Modify DataType." "Remove Column." "Exit"; do
+    select reply in "Add New Column." "Modify DataType." "Remove Column." "List column names." "Exit"; do
         case $REPLY in
         1)
             addNewColumn $tname
@@ -157,6 +165,11 @@ updateMenu() {
             return
             ;;
         4)
+            lsColNames $tname
+            updateMenu $tname
+            return
+            ;;
+        5)
             return
             ;;
         *)
